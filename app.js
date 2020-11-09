91b830443a0a648ae3e15b772b1e28a065d1e959
italopussi <italopussi@gmail.com>
2020-11-09 12:56:33 -0300

    NavBar finalized, and Presentation

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const body = document.querySelector("body")
 const nav = document.querySelector("nav")
 const links = nav.querySelectorAll("li")
 
 const toggleBar = document.querySelector("#toggleBar")
 
-toggleBar.addEventListener("change",()=>nav.classList.toggle("open"))
+toggleBar.addEventListener("change",()=>{
+	nav.classList.toggle("open")
+
+})
 links.forEach(link=>link.addEventListener("mouseover", ()=>nav.style.color="grey"))
 links.forEach(link=>link.addEventListener("mouseout", ()=>nav.style.color="#e2e2e2"))
