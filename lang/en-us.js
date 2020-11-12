df2a801e682441c205751a51b0a58201f2c1e400
ItaloPussi <italopussi@gmail.com>
2020-11-12 18:37:58 -0300

    Favicon and design fixes

--- lang/en-us.js
+++ lang/en-us.js
@@ -8,6 +8,6 @@ const langEn =  {
     "much": "And much more!",
     "seePort": "See portfolio",
     "plus": "PLUS MORE",
-    "wannaSeeMore":"Wanna see more projects like this?",
-    "wannaSeeMoreText":"If so, feel free to access my Github. Now and then new projects are made or atualized"
+    "wannaSeeMore":"Wanna see more projects like those?",
+    "wannaSeeMoreText":"If so, feel free to access my Github. Now and then new projects are made or atualized."
 }
